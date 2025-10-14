b674df2d9bc5583352e6af352fc8eeb1225ec46a
Taehong Kim <peppy0510@hotmail.com>
2025-10-14 22:47:37 +0900

    update

--- README.md
+++ README.md
@@ -1,17 +1,15 @@
-## ZSH Auto Activate Python VENV
-
-### Description
+### ZSH VENV Auto
 
 * Automaticall activate and deactivate Python Virtual Environment when you change directory
 * Installed Virtual Environment directory name must be one of `venv`, `env`, `.venv`, `.env`
-* oh-my-zsh should be installed
+* oh-my-zsh must be installed
 
 ### Installation
 
 ```bash
-git clone https://github.com/peppy0510/zsh-auto-activate-python-venv.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-auto-activate-python-venv
+git clone https://github.com/peppy0510/zsh-venv-auto.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-venv-auto
 ```
-* Add `zsh-auto-activate-python-venv` to `~/.zshrc` `plugins` section.
+* Add `zsh-venv-auto` to `~/.zshrc` `plugins` section.
 
 ```bash
 vim ~/.zshrc
@@ -19,6 +17,6 @@ vim ~/.zshrc
 
 ```bash
 plugins=(
-    zsh-auto-activate-python-venv
+    zsh-venv-auto
 )
 ```
---
+++ zsh-venv-auto.zsh
@@ -0,0 +1,47 @@
+# author: Taehong Kim
+# email: peppy0510@hotmail.com
+
+ZSH_ACTIVATED_VIRTUAL_ENV=""
+
+auto_activate_venv() {
+    VIRTUALENV_NAMES=("venv" "env" ".venv" ".env")
+    VIRTUALENV_BINDIRS=("bin" "Scripts")
+    current="$PWD"
+    if [[ -n "$VIRTUAL_ENV" && -z "$ZSH_ACTIVATED_VIRTUAL_ENV" ]]; then
+        for bindir in "${VIRTUALENV_BINDIRS[@]}"; do
+            if [[ -f "$current/$name/${bindir}/activate" ]]; then
+                source "$VIRTUAL_ENV/bin/activate" && deactivate
+            fi
+        done
+    fi
+    while [[ "$current" != "/" ]]; do
+        for name in "${VIRTUALENV_NAMES[@]}"; do
+            for bindir in "${VIRTUALENV_BINDIRS[@]}"; do
+                if [[ -d "$current/$name" && -f "$current/$name/${bindir}/activate" ]]; then
+                    if [[ "$VIRTUAL_ENV" != "$current/$name" ]]; then
+                        [[ -n "$VIRTUAL_ENV" ]] && deactivate
+                        source "$current/$name/${bindir}/activate"
+                        ZSH_ACTIVATED_VIRTUAL_ENV="$current/$name/${bindir}/activate"
+                    fi
+                    return
+                fi
+            done
+        done
+        current="$(dirname "$current")"
+    done
+    [[ -n "$VIRTUAL_ENV" ]] && deactivate
+}
+
+cd() {
+    builtin cd "$@" && auto_activate_venv
+}
+
+pushd() {
+    builtin pushd "$@" && auto_activate_venv
+}
+
+popd() {
+    builtin popd "$@" && auto_activate_venv
+}
+
+auto_activate_venv
